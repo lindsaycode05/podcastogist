e5f0c9d9e1f6eea614f4b599626a5a1f69e11140
Lindsay <lindsaycode05@gmail.com>
2025-12-08 14:34:45 +0200

    Implement the Hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from '@/components/header';
+import { HeroSection } from '@/components/home/hero-section';
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <HeroSection />
     </div>
   );
 }
